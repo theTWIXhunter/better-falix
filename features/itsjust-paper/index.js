6dfa140c87c26290297828a2cd12614819927509
theTWIXhunter <147813783+theTWIXhunter@users.noreply.github.com>
2025-06-07 20:35:52 +0200

    troubleshoot

--- features/itsjust-paper/index.js
+++ features/itsjust-paper/index.js
@@ -1,4 +1,8 @@
+// [itsjust-paper] Make sure this script is loaded and runs on the correct page!
+console.log('[itsjust-paper] Script loaded');
+
 chrome.storage.sync.get({ enabled: true, itsjustPaper: false }, (data) => {
+  console.log('[itsjust-paper] chrome.storage.sync.get', data);
   if (!data.enabled || !data.itsjustPaper) {
     console.log('[itsjust-paper] Feature or extension disabled');
     return;
