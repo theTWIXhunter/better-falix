be053f9f0a73323a5de96116d4842b45de9ce2e7
theTWIXhunter <147813783+theTWIXhunter@users.noreply.github.com>
2025-09-20 17:15:57 +0200

    remove all tests

--- popup.js
+++ popup.js
@@ -41,8 +41,7 @@ const featureIds = [
   'autoCollapseLogAnalysis',
   'moveAdminPanelNav',
   'adminPanelIndexRedirect',
-  'removePremiumRow',
-  'enhancedReplyPanel'
+  'removePremiumRow'
 ];
 
 function setFeatureBtnState(btn, enabled) {
@@ -136,8 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
     autoCollapseLogAnalysis: false,
     moveAdminPanelNav: false,
     adminPanelIndexRedirect: false,
-    removePremiumRow: false,
-    enhancedReplyPanel: false
+    removePremiumRow: false
 
   }, (data) => {
     updateToggleBtn(data.enabled);
