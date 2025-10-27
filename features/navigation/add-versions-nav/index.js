69fd8ce2272bb89c2c522ae0b6c6abb84f6a8905
theTWIXhunter <147813783+theTWIXhunter@users.noreply.github.com>
2025-10-27 20:51:14 +0100

    fix styling

--- features/navigation/add-versions-nav/index.js
+++ features/navigation/add-versions-nav/index.js
@@ -33,7 +33,11 @@ chrome.storage.sync.get({ addVersionsNav: false, enabled: true }, (data) => {
       return;
     }
 
-    // Create the Versions nav item
+    // Create the Versions nav item wrapped in li
+    const listItem = document.createElement('li');
+    listItem.className = 'nav-item';
+    listItem.setAttribute('role', 'listitem');
+    
     const versionsNavItem = document.createElement('a');
     versionsNavItem.className = 'nav-link';
     versionsNavItem.href = '/server/versions';
@@ -42,13 +46,22 @@ chrome.storage.sync.get({ addVersionsNav: false, enabled: true }, (data) => {
       <svg class="svg-inline--fa fa-code-branch" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="code-branch" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M80 104a24 24 0 1 0 0-48 24 24 0 1 0 0 48zm80-24c0 32.8-19.7 61-48 73.3l0 38.7c0 17.7 14.3 32 32 32l160 0c17.7 0 32-14.3 32-32l0-38.7C307.7 141 288 112.8 288 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3l0 38.7c0 53-43 96-96 96l-48 0 0 70.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3l0-70.7-48 0c-53 0-96-43-96-96l0-38.7C19.7 141 0 112.8 0 80C0 35.8 35.8 0 80 0s80 35.8 80 80zm208 24a24 24 0 1 0 0-48 24 24 0 1 0 0 48zM248 432a24 24 0 1 0 -48 0 24 24 0 1 0 48 0z"></path></svg>
       <span>Versions</span>
     `;
+    
+    listItem.appendChild(versionsNavItem);
 
     // Find the collapsible content area (minecraftSection)
     const minecraftSection = document.getElementById('minecraftSection');
     if (minecraftSection) {
-      // Add it as the first item in the Minecraft section
-      minecraftSection.insertBefore(versionsNavItem, minecraftSection.firstChild);
-      console.log('[better-falix] add-versions-nav: Versions nav item added successfully');
+      // Find the navbar-nav ul inside
+      const navbarNav = minecraftSection.querySelector('ul.navbar-nav');
+      
+      if (navbarNav) {
+        // Add it as the first item in the list
+        navbarNav.insertBefore(listItem, navbarNav.firstChild);
+        console.log('[better-falix] add-versions-nav: Versions nav item added successfully');
+      } else {
+        console.log('[better-falix] add-versions-nav: navbar-nav not found');
+      }
     } else {
       console.log('[better-falix] add-versions-nav: Minecraft section content not found');
     }
